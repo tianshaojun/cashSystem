814bac93c016894157a1f04569ea90e273620fa9
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2020-09-03 16:06:19 +0800

    项目代码提交

---
+++ README.md
@@ -0,0 +1,25 @@
+# cashSystem
+
+> 这是为老婆作的快餐店管理系统，已经答应老婆作好几年了都没给他作，今天开始要为老婆制作这个系统。
+
+## 技术栈
+
+前端 ：vue2 + webpack + vueRouter + element
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!--左侧导航-->
+    <leftNav></leftNav>
+
+    <!--主体区域-->
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import leftNav from "@/components/common/leftNav";
+export default {
+  name: "app",
+  components: {
+    leftNav,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Microsoft YaHei", "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  height: 100%;
+}
+
+.main {
+  float: left;
+  width: 95%;
+  background-color: #eff2f7;
+  height: 100%;
+  overflow: auto;
+}
+</style>
