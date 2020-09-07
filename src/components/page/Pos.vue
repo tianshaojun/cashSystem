ef5d4d527da2f593f663e5cfca0a393f7baf7817
tianshaojun <2467307672@qq.com>
2020-09-07 21:09:53 +0800

    不同方式实现轮播

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,128 +1,67 @@
 <template>
-  <div class="thumb-example">
-    <!-- swiper1 -->
-    <swiper
-      class="swiper gallery-top"
-      :options="swiperOptionTop"
-      ref="swiperTop"
-    >
-      <swiper-slide class="slide-1"></swiper-slide>
-      <swiper-slide class="slide-2"></swiper-slide>
-      <swiper-slide class="slide-3"></swiper-slide>
-      <swiper-slide class="slide-4"></swiper-slide>
-      <swiper-slide class="slide-5"></swiper-slide>
-      <div
-        class="swiper-button-next swiper-button-white"
-        slot="button-next"
-      ></div>
-      <div
-        class="swiper-button-prev swiper-button-white"
-        slot="button-prev"
-      ></div>
-    </swiper>
-    <!-- swiper2 Thumbs -->
-    <swiper
-      class="swiper gallery-thumbs"
-      :options="swiperOptionThumbs"
-      ref="swiperThumbs"
-    >
-      <swiper-slide class="slide-1"></swiper-slide>
-      <swiper-slide class="slide-2"></swiper-slide>
-      <swiper-slide class="slide-3"></swiper-slide>
-      <swiper-slide class="slide-4"></swiper-slide>
-      <swiper-slide class="slide-5"></swiper-slide>
-    </swiper>
+  <div id="app">
+    <div>
+      <swiper :options="swiperOption">
+        <swiper-slide class="swiper-slide" v-for="(item,index) in slide" :key="index">
+          <img :src="item.url" alt="">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>   <!-- 分页 -->
+        <div class="swiper-button-prev" slot="button-prev"></div>  <!-- 箭头左 --> 
+        <div class="swiper-button-next" slot="button-next"></div>  <!-- 箭头右 -->
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-// import "swiper/css/swiper.css";
-// import "swiper/swiper-bundle.css";
-import "swiper/dist/css/swiper.css";
-
 export default {
-  name: "swiper-example-thumbs-gallery",
-  title: "Thumbs gallery with Two-way control",
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
+  name: 'App',
   data() {
     return {
-      swiperOptionTop: {
-        loop: true,
-        loopedSlides: 5, // looped slides should be the same
-        spaceBetween: 10,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
+      slide: [
+        {
+          url: require("../../assets/md_img/11.jpg"),
+          id: '11'
         },
-      },
-      swiperOptionThumbs: {
+        {
+          url: require("../../assets/md_img/22.jpg"),
+          id: '22'
+        },
+      ],
+      //设置属性
+      swiperOption: {
+        //显示分页
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true //允许分页点击跳转
+        },
+        //设置点击箭头
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        //自动轮播
+        autoplay: {
+          delay: 4000
+        },
+        //开启循环模式
         loop: true,
-        loopedSlides: 5, // looped slides should be the same
-        spaceBetween: 10,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        touchRatio: 0.2,
-        slideToClickedSlide: true,
-        // observer: true, //修改swiper自己或子元素时，自动初始化swiper
-        // observeParents: true,
-      },
-    };
-  },
-  mounted() {
-    this.$nextTick(() => {
-      const swiperTop = this.$refs.swiperTop.swiper;
-      const swiperThumbs = this.$refs.swiperThumbs.swiper;
-      swiperTop.controller.control = swiperThumbs;
-      swiperThumbs.controller.control = swiperTop;
-    });
-  },
-};
+        //开启鼠标滚轮控制Swiper切换
+        mousewheel: true
+      }
+    }
+  }
+}
 </script>
 
-<style scoped>
-.thumb-example {
-  height: 480px;
-  background-color: pink;
+<style>
+.swiper-slide {
+  width: 100%;
+  height: 500px;
 }
 
-.swiper .swiper-slide {
-  background-size: cover;
-  background-position: center;
-}
-.swiper .swiper-slide.slide-1 {
-  background-image: url("../../assets/logo.png");
-}
-.swiper .swiper-slide.slide-2 {
-  background-image: url("../../assets/logo.png");
-}
-.swiper .swiper-slide.slide-3 {
-  background-image: url("../../assets/logo.png");
-}
-.swiper .swiper-slide.slide-4 {
-  background-image: url("../../assets/logo.png");
-}
-.swiper .swiper-slide.slide-5 {
-  background-image: url("../../assets/logo.png");
-}
-.swiper.gallery-top {
-  height: 80%;
+.swiper-slide img{
   width: 100%;
-}
-.swiper.gallery-thumbs {
-  height: 20%;
-  box-sizing: border-box;
-  padding: 10px 0;
-}
-.swiper.gallery-thumbs .swiper-slide {
-  width: 25%;
-  height: 100%;
-  opacity: 0.4;
-}
-.swiper.gallery-thumbs .swiper-slide-active {
-  opacity: 1;
+  height: 500px;
 }
 </style>
---
+++ src/components/page/ele.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-carousel :interval="5000" arrow="always">
+    <el-carousel-item v-for="item in imgs" :key="item.id">
+      <img :src="item.img" alt="">
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgs: [
+        {
+          img: require("../../assets/md_img/11.jpg"),
+          id: '111'
+        },
+        {
+          img: require("../../assets/md_img/22.jpg"),
+          id: '222'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .el-carousel__item img {
+    width: 100%;
+    height: 300px;
+  }
+</style>
