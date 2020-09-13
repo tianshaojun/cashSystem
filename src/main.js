79224b26045790da881a0e9206f52d9318cee177
田少军 <tianshaojun@tianshaojundeMacBook-Pro.local>
2020-09-13 17:52:20 +0800

    20200913项目打包有问题

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-default/index.css";
 
-import VueAwesomeSwiper from "vue-awesome-swiper";
-import "swiper/dist/css/swiper.css";
-
-Vue.use(VueAwesomeSwiper /* { default global options } */);
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
